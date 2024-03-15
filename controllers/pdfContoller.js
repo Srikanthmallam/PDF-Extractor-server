9913ef649a3afeec66870d1a982f4239d24d4bb9
srikanthmallam <sriaknthmallam0@gmail.com>
2024-03-15 08:52:23 +0530

    .

--- controllers/pdfContoller.js
+++ controllers/pdfContoller.js
@@ -1,9 +1,9 @@
-const { PDFDocument, PDFDocumentFactory } = require("pdf-lib");
+const { PDFDocument } = require("pdf-lib");
 const cloudinary = require("cloudinary").v2;
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const PDF = require("../models/psfModel");
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 const HttpError = require("../models/errorModel");
 const path = require("path");
 
@@ -25,8 +25,8 @@ const uploadPdf = async (req, res, next) => {
       return next(new HttpError("PDF file is required"));
     }
 
-    if(!req.body.pageNumbers){
-      return next(new HttpError("please select atleast one page",422))
+    if (!req.body.pageNumbers) {
+      return next(new HttpError("please select atleast one page", 422));
     }
 
     const pageNumberString = req.body.pageNumbers;
@@ -68,7 +68,7 @@ const uploadPdf = async (req, res, next) => {
     if (authToken) {
       const token = authToken.split(" ")[1];
       const decodedToken = jwt.verify(token, process.env.jWT_SECRET);
-      const userId = decodedToken.id;
+      const userId = decodedToken?.id;
 
       cloudinary.uploader.upload(
         localFilePath,
@@ -77,14 +77,12 @@ const uploadPdf = async (req, res, next) => {
           if (error) {
             console.error("Error uploading PDF:", error);
           } else {
-
             const newPdf = new PDF({
               name: processedFileName,
               url: result.secure_url,
               owner: userId,
-              cloudinaryId:result.public_id,
+              cloudinaryId: result.public_id,
             });
-
 
             await newPdf.save();
 
@@ -98,50 +96,49 @@ const uploadPdf = async (req, res, next) => {
       );
     } else {
       processedFileUrl = `/uploads/${processedFileName}`;
-      res.status(200).json({processedFileUrl: processedFileUrl});
+      res.status(200).json({ processedFileUrl: processedFileUrl });
     }
   } catch (error) {
     return next(new HttpError("internal server Error in :", error));
   }
 };
 
-const userPdfs = async(req,res,next)=>{
-    try {
-        const {id} = req.params;
+const userPdfs = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-        const user = await User.findById(id).select("-password");
-        if(!user){
-            return next(new HttpError("user not found",422));
-        }
-
-        const pdfs = await PDF.find({owner:id});
-        if(!pdfs){
-            return next(new HttpError("No files found",422));
-        }
-        res.status(200).json({pdfs});
-
-    } catch (error) {
-        return next(new HttpError(error) );
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      return next(new HttpError("user not found", 422));
     }
-}
 
-const deletePdf = async(req,res,next)=>{
-    try {
-        const {id} = req.params;
-        const pdf = await PDF.findById(id);
-
-        if(!pdf){
-            return next(new HttpError("Pdf Not Found",404));
-        }
-        
-        await cloudinary.uploader.destroy(pdf.cloudinaryId);
-
-        await PDF.findByIdAndDelete(id);
-
-        res.status(200).json("pdf fiel deleted")
-    } catch (error) {
-        return next(new HttpError("internal server Error",500))
+    const pdfs = await PDF.find({ owner: id });
+    if (!pdfs) {
+      return next(new HttpError("No files found", 422));
     }
-}
+    res.status(200).json({ pdfs });
+  } catch (error) {
+    return next(new HttpError(error));
+  }
+};
 
-module.exports = { uploadPdf,userPdfs,deletePdf};
+const deletePdf = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const pdf = await PDF.findById(id);
+
+    if (!pdf) {
+      return next(new HttpError("Pdf Not Found", 404));
+    }
+
+    await cloudinary.uploader.destroy(pdf.cloudinaryId);
+
+    await PDF.findByIdAndDelete(id);
+
+    res.status(200).json("pdf fiel deleted");
+  } catch (error) {
+    return next(new HttpError("internal server Error", 500));
+  }
+};
+
+module.exports = { uploadPdf, userPdfs, deletePdf };
